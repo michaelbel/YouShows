b5c5149c068e6a77690c8fc9024784773e443159
Michael Bel <michael-bel@outlook.com>
2018-03-29 19:47:27 +0400

    Update activity

--- app/src/main/java/org/michaelbel/ui/MainActivity.java
+++ app/src/main/java/org/michaelbel/ui/MainActivity.java
@@ -1,22 +1,42 @@
 package org.michaelbel.ui;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
+import org.michaelbel.material.widget2.FragmentsPagerAdapter;
+import org.michaelbel.old.Theme;
 import org.michaelbel.seriespicker.R;
-import org.michaelbel.seriespicker.Theme;
-import org.michaelbel.ui.fragment.MainFragment;
+import org.michaelbel.ui.fragment.FollowingFragment;
+import org.michaelbel.ui.fragment.MyShowsFragment;
 
-import me.yokeyword.fragmentation.anim.DefaultHorizontalAnimator;
-import me.yokeyword.fragmentation.anim.FragmentAnimator;
-import me.yokeyword.fragmentation_swipeback.SwipeBackActivity;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends SwipeBackActivity {
+    private final int NOTIFICATION_ICON_INDEX = 1;
+
+    private final int tab_shows = 0;
+    private final int tab_follow = 1;
+
+    private boolean iconNotificationMode;
+
+    private Menu menu;
+    private ViewPager viewPager;
 
     public Toolbar toolbar;
-    public TextView toolbarTextView;
+    public TabLayout tabLayout;
+    public FloatingActionButton floatingButton;
+
+    private MyShowsFragment myShowsFragment;
+    private FollowingFragment followingEpisodesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,37 +44,107 @@ public class MainActivity extends SwipeBackActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
-        toolbar.setPopupTheme(Theme.popupTheme());
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+        floatingButton = findViewById(R.id.fab);
+
         setSupportActionBar(toolbar);
 
-        toolbarTextView = findViewById(R.id.toolbar_title);
+        myShowsFragment = new MyShowsFragment();
+        followingEpisodesFragment = new FollowingFragment();
 
-        if (findFragment(MainFragment.class) == null) {
-            loadRootFragment(R.id.fragment_layout, new MainFragment());
+        FragmentsPagerAdapter adapter = new FragmentsPagerAdapter(this, getSupportFragmentManager());
+        adapter.addFragment(myShowsFragment, R.string.MyShows);
+        //adapter.addFragment(followingEpisodesFragment, R.string.Following);
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                notificationIconVisibility(position);
+
+                if (position == tab_shows) {
+                    floatingButton.show();
+                } else if (position == tab_follow) {
+                    floatingButton.hide();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setBackgroundColor(ContextCompat.getColor(this, Theme.primaryColor()));
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.primaryText));
+        tabLayout.setTabTextColors(ContextCompat.getColor(this, R.color.secondaryText), ContextCompat.getColor(this, R.color.primaryText));
+
+        floatingButton.setImageResource(R.drawable.ic_plus);
+        floatingButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ExploreActivity.class)));
+
+        SharedPreferences prefs = getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
+        iconNotificationMode = prefs.getBoolean("following_notifications", true);
+
+        // В настройки добавить какую страницу показывать при запуске.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+
+        menu.add(R.string.Search)
+            .setIcon(R.drawable.ic_search)
+            .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+            .setOnMenuItemClickListener(item -> {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                return true;
+            });
+
+        menu.add(null)
+            .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+            .setOnMenuItemClickListener(item -> {
+                if (viewPager.getCurrentItem() == tab_shows) {
+                    startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                } else if (viewPager.getCurrentItem() == tab_follow) {
+                    SharedPreferences prefs = getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean("following_notifications", !iconNotificationMode);
+                    editor.apply();
+
+                    followingEpisodesFragment.showSnackbar(!iconNotificationMode);
+                    iconNotificationMode = prefs.getBoolean("following_notifications", true);
+                    changeNotificationIcon();
+                }
+
+                return true;
+            });
+
+        notificationIconVisibility(viewPager.getCurrentItem());
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void notificationIconVisibility(int position) {
+        if (menu != null) {
+            if (position == tab_follow) {
+                menu.getItem(NOTIFICATION_ICON_INDEX).setIcon(iconNotificationMode ? R.drawable.ic_notifications_active : R.drawable.ic_notifications_off);
+            } else {
+                menu.getItem(NOTIFICATION_ICON_INDEX).setIcon(R.drawable.ic_settings);
+            }
         }
-
-        getSwipeBackLayout().setEnableGesture(false);
     }
 
-    @Override
-    public void setTheme(int resid) {
-        super.setTheme(Theme.getTheme() ? R.style.ThemeLight : R.style.ThemeNight);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+    private void changeNotificationIcon() {
+        if (menu != null) {
+            if (iconNotificationMode) {
+                menu.getItem(NOTIFICATION_ICON_INDEX).setIcon(R.drawable.ic_notifications_active);
+            } else {
+                menu.getItem(NOTIFICATION_ICON_INDEX).setIcon(R.drawable.ic_notifications_off);
+            }
         }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean swipeBackPriority() {
-        return super.swipeBackPriority();
-    }
-
-    public FragmentAnimator onCreateFragmentAnimator() {
-        return new DefaultHorizontalAnimator();
     }
 }
