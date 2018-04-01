5ffec8381f9c3dc807bd5c402ae139507eda21d2
Michael Bel <michael-bel@outlook.com>
2018-04-02 00:34:34 +0400

    Minor updates

---
+++ app/src/main/java/org/michaelbel/rest/model/Company.java
@@ -0,0 +1,29 @@
+package org.michaelbel.rest.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import org.michaelbel.rest.TmdbObject;
+
+import java.io.Serializable;
+
+/**
+ * Date: 02 APR 2018
+ * Time: 00:33 MSK
+ *
+ * @author Michael Bel
+ */
+
+public class Company extends TmdbObject implements Serializable {
+
+    @SerializedName("id")
+    public int id;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("logo_path")
+    public String logoPath;
+
+    @SerializedName("origin_country")
+    public String originCountry;
+}
