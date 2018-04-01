bc54ce49754009b5eb42f4a2695beb8f72c578f1
Michael Bel <michael-bel@outlook.com>
2018-04-01 19:05:50 +0400

    Minor updates

---
+++ app/src/main/java/org/michaelbel/ui/SearchActivity.java
@@ -0,0 +1,195 @@
+package org.michaelbel.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+
+import org.michaelbel.material.extensions.Extensions;
+import org.michaelbel.old.LayoutHelper;
+import org.michaelbel.seriespicker.R;
+import org.michaelbel.ui.fragment.SearchFragment;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Date: 17 MAR 2018
+ * Time: 13:19 MSK
+ *
+ * @author Michael Bel
+ */
+
+public class SearchActivity extends AppCompatActivity {
+
+    private final int MENU_ITEM_INDEX = 0;
+    private final int SPEECH_REQUEST_CODE = 101;
+
+    private final int MODE_ACTION_CLEAR = 1;
+    private final int MODE_ACTION_VOICE = 2;
+
+    private int iconActionMode;
+
+    private Menu actionMenu;
+    private EditText searchEditText;
+    private SearchFragment searchFragment;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(view -> finish());
+
+        iconActionMode = MODE_ACTION_VOICE;
+
+        FrameLayout toolbarLayout = new FrameLayout(this);
+        toolbarLayout.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        toolbar.addView(toolbarLayout);
+
+        searchEditText = new EditText(this);
+        searchEditText.setLines(1);
+        searchEditText.setMaxLines(1);
+        searchEditText.setSingleLine();
+        searchEditText.setHint(R.string.Search);
+        searchEditText.setBackground(null);
+        //searchEditText.setBackgroundDrawable(null);
+        searchEditText.setTypeface(Typeface.DEFAULT);
+        searchEditText.setEllipsize(TextUtils.TruncateAt.END);
+        searchEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+        searchEditText.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        searchEditText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        searchEditText.setTextColor(ContextCompat.getColor(this, R.color.md_white));
+        searchEditText.setHintTextColor(ContextCompat.getColor(this, R.color.night_disabledHintTextColor));
+        searchEditText.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT, Gravity.START | Gravity.CENTER_VERTICAL));
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence text, int start, int before, int count) {
+                if (!TextUtils.isEmpty(text)) {
+                    searchFragment.search(text.toString().trim());
+                }
+                changeActionIcon();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+        searchEditText.setOnEditorActionListener((view, actionId, event) -> {
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_SEARCH)) {
+                searchFragment.search(view.getText().toString().trim());
+                hideKeyboard(searchEditText);
+                return true;
+            }
+
+            return false;
+        });
+        toolbarLayout.addView(searchEditText);
+        Extensions.clearCursorDrawable(searchEditText);
+
+        searchFragment = (SearchFragment) getSupportFragmentManager().findFragmentById(R.id.searchFragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.actionMenu = menu;
+
+        menu.add(null)
+            .setIcon(R.drawable.ic_mic)
+            .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+            .setOnMenuItemClickListener(menuItem -> {
+                if (iconActionMode == MODE_ACTION_VOICE) {
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+                    intent.putExtra(RecognizerIntent.EXTRA_PROMPT, R.string.SpeakNow);
+                    startActivityForResult(intent, SPEECH_REQUEST_CODE);
+                } else if (iconActionMode == MODE_ACTION_CLEAR) {
+                    searchEditText.getText().clear();
+                    changeActionIcon();
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT);
+                    }
+                }
+                return true;
+            });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SPEECH_REQUEST_CODE) {
+            if (resultCode == RESULT_OK && data != null) {
+                List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (results != null && results.size() > 0) {
+                    String textResults = results.get(0);
+                    if (!TextUtils.isEmpty(textResults)) {
+                        if (searchEditText != null) {
+                            searchEditText.setText(textResults);
+                            searchEditText.setSelection(searchEditText.getText().length());
+                            changeActionIcon();
+                            searchFragment.search(textResults);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void changeActionIcon() {
+        if (actionMenu != null) {
+            if (searchEditText.getText().toString().trim().isEmpty()) {
+                iconActionMode = MODE_ACTION_VOICE;
+                actionMenu.getItem(MENU_ITEM_INDEX).setIcon(R.drawable.ic_mic);
+            } else {
+                iconActionMode = MODE_ACTION_CLEAR;
+                actionMenu.getItem(MENU_ITEM_INDEX).setIcon(R.drawable.ic_clear);
+            }
+        }
+    }
+
+    private void hideKeyboard(View view) {
+        if (view == null) {
+            return;
+        }
+
+        try {
+            InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (!imm.isActive()) {
+                return;
+            }
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/org/michaelbel/ui/fragment/SearchFragment.java
@@ -0,0 +1,223 @@
+package org.michaelbel.ui.fragment;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import org.michaelbel.app.AndroidExtensions;
+import org.michaelbel.old.LayoutHelper;
+import org.michaelbel.old.ui_old.view.RecyclerListView;
+import org.michaelbel.rest.ApiFactory;
+import org.michaelbel.rest.ApiService;
+import org.michaelbel.rest.TmdbObject;
+import org.michaelbel.rest.model.Show;
+import org.michaelbel.rest.response.ShowsResponse;
+import org.michaelbel.seriespicker.R;
+import org.michaelbel.ui.SearchActivity;
+import org.michaelbel.ui.ShowActivity;
+import org.michaelbel.ui.adapter.PaginationShowsAdapter;
+import org.michaelbel.ui.view.EmptyView;
+import org.michaelbel.ui.view.EmptyViewMode;
+import org.michaelbel.ui.view.ShowView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Date: 17 MAR 2018
+ * Time: 13:40 MSK
+ *
+ * @author Michael Bel
+ */
+
+public class SearchFragment extends Fragment {
+
+    public int page = 1;
+    public int totalPages;
+    public int totalResults;
+    public boolean isLoading = false;
+    public boolean isLastPage = false;
+
+    private String searchQuery;
+    private SearchActivity activity;
+    private PaginationShowsAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+
+    private EmptyView emptyView;
+    private ProgressBar progressBar;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = (SearchActivity) getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FrameLayout fragmentView = new FrameLayout(activity);
+        fragmentView.setBackgroundColor(ContextCompat.getColor(activity, R.color.background));
+
+        emptyView = new EmptyView(activity);
+        emptyView.setMode(EmptyViewMode.MODE_NO_RESULTS);
+        emptyView.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, Gravity.CENTER, 24, 0, 24, 0));
+        fragmentView.addView(emptyView);
+
+        progressBar = new ProgressBar(activity);
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(activity, R.color.accent), PorterDuff.Mode.MULTIPLY);
+        progressBar.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, Gravity.CENTER));
+        fragmentView.addView(progressBar);
+
+        adapter = new PaginationShowsAdapter();
+        linearLayoutManager = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
+
+        RecyclerListView recyclerView = new RecyclerListView(activity);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setEmptyView(emptyView);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setLayoutParams(LayoutHelper.makeFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        recyclerView.setOnItemClickListener((view, position) -> {
+            if (view instanceof ShowView) {
+                Show show = (Show) adapter.getList().get(position);
+
+                Intent intent = new Intent(activity, ShowActivity.class);
+                intent.putExtra("show", show);
+                startActivity(intent);
+            }
+        });
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int totalItemCount = linearLayoutManager.getItemCount();
+                int visibleItemCount = linearLayoutManager.getChildCount();
+                int firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition();
+
+                if (!isLoading && !isLastPage) {
+                    if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0/* && totalItemCount >= totalPages*/) {
+                        isLoading = true;
+                        page++;
+                        loadNext();
+                    }
+                }
+            }
+        });
+        fragmentView.addView(recyclerView);
+        return fragmentView;
+    }
+
+    public void search(String query) {
+        searchQuery = query;
+        searchStart();
+
+        if (AndroidExtensions.typeNotConnected()) {
+            showError(EmptyViewMode.MODE_NO_CONNECTION);
+            return;
+        }
+
+        ApiService service = ApiFactory.createService(ApiService.class, ApiFactory.TMDB_API_ENDPOINT);
+        service.search(ApiFactory.TMDB_API_KEY, ApiFactory.TMDB_EN_US, searchQuery, page).enqueue(new Callback<ShowsResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<ShowsResponse> call, @NonNull Response<ShowsResponse> response) {
+                if (response.isSuccessful()) {
+                    ShowsResponse showsResponse = response.body();
+                    if (showsResponse != null) {
+                        totalPages = showsResponse.totalPages;
+                        totalResults = showsResponse.totalResults;
+                        List<TmdbObject> results = new ArrayList<>(showsResponse.shows);
+                        if (results.isEmpty()) {
+                            showError(EmptyViewMode.MODE_NO_RESULTS);
+                            return;
+                        }
+                        showResults(results, true);
+                    }
+                } else {
+                    showError(EmptyViewMode.MODE_NO_RESULTS);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ShowsResponse> call, @NonNull Throwable t) {
+                showError(EmptyViewMode.MODE_NO_CONNECTION);
+            }
+        });
+    }
+
+    private void loadNext() {
+        ApiService service = ApiFactory.createService(ApiService.class, ApiFactory.TMDB_API_ENDPOINT);
+        service.search(ApiFactory.TMDB_API_KEY, ApiFactory.TMDB_EN_US, searchQuery, page).enqueue(new Callback<ShowsResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<ShowsResponse> call, @NonNull Response<ShowsResponse> response) {
+                if (response.isSuccessful()) {
+                    ShowsResponse showsResponse = response.body();
+                    if (showsResponse != null) {
+                        List<TmdbObject> results = new ArrayList<>(showsResponse.shows);
+                        showResults(results, false);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ShowsResponse> call, @NonNull Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    public void searchStart() {
+        adapter.getList().clear();
+        adapter.notifyDataSetChanged();
+
+        emptyView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void showResults(List<TmdbObject> results, boolean firstPage) {
+        if (firstPage) {
+            progressBar.setVisibility(View.GONE);
+
+            adapter.addAll(results);
+
+            if (page < totalPages) {
+                adapter.addLoadingFooter();
+            } else {
+                isLastPage = true;
+            }
+        } else {
+            adapter.removeLoadingFooter();
+            isLoading = false;
+            adapter.addAll(results);
+
+            if (page != totalPages) {
+                adapter.addLoadingFooter();
+            } else {
+                isLastPage = true;
+            }
+        }
+    }
+
+    public void showError(int mode) {
+        progressBar.setVisibility(View.GONE);
+        emptyView.setVisibility(View.VISIBLE);
+        emptyView.setMode(mode);
+    }
+}
