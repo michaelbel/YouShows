8120ce9cc18e49ca61b435e858d75677e2e1964c
Michael Bel <michael-bel@outlook.com>
2018-04-02 22:03:48 +0400

    Minor updates

---
+++ app/src/main/java/org/michaelbel/ui/SeasonActivity.java
@@ -0,0 +1,49 @@
+package org.michaelbel.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import org.michaelbel.realm.ShowRealm;
+import org.michaelbel.rest.model.Season;
+import org.michaelbel.seriespicker.R;
+import org.michaelbel.ui.fragment.SeasonFragment;
+
+/**
+ * Date: 19 MAR 2018
+ * Time: 13:20 MSK
+ *
+ * @author Michael Bel
+ */
+
+public class SeasonActivity extends AppCompatActivity {
+
+    public Season season;
+    public ShowRealm showRealm;
+
+    public Toolbar toolbar;
+    public TextView toolbarTitle;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_season);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+
+        toolbar.setNavigationIcon(R.drawable.ic_clear);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(v -> finish());
+
+        showRealm = getIntent().getParcelableExtra("showRealm");
+        season = (Season) getIntent().getSerializableExtra("season");
+
+        toolbarTitle.setText(season.name);
+
+        SeasonFragment fragment = (SeasonFragment) getSupportFragmentManager().findFragmentById(R.id.seasonFragment);
+        fragment.showEpisodes(showRealm.showId, season.seasonNumber);
+    }
+}
