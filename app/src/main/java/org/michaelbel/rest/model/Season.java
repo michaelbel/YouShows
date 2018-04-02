e24d1c20b4cd105abad2449f6ec3d824eba3c441
Michael Bel <michael-bel@outlook.com>
2018-04-02 21:55:45 +0400

    Minor updates

--- app/src/main/java/org/michaelbel/rest/model/Season.java
+++ app/src/main/java/org/michaelbel/rest/model/Season.java
@@ -2,6 +2,7 @@ package org.michaelbel.rest.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import org.michaelbel.rest.Response;
 import org.michaelbel.rest.TmdbObject;
 
 import java.io.Serializable;
@@ -15,8 +16,25 @@ import java.io.Serializable;
 
 public class Season extends TmdbObject implements Serializable {
 
+    @Response
     @SerializedName("id")
     public int id;
+
+    @Response
+    @SerializedName("air_date")
+    public String airDate;
+
+    @Response
+    @SerializedName("poster_path")
+    public String posterPath;
+
+    @Response
+    @SerializedName("episode_count")
+    public int episodeCount;
+
+    @Response
+    @SerializedName("season_number")
+    public int seasonNumber;
 
     @SerializedName("name")
     public String name;
@@ -24,15 +42,4 @@ public class Season extends TmdbObject implements Serializable {
     @SerializedName("overview")
     public String overview;
 
-    @SerializedName("air_date")
-    public String airDate;
-
-    @SerializedName("poster_path")
-    public String posterPath;
-
-    @SerializedName("episode_count")
-    public int episodeCount;
-
-    @SerializedName("season_number")
-    public int seasonNumber;
 }
