e8a2f32ce2e36296528994fc0c263ec563d4bd67
GitHub <noreply@github.com>
2018-04-06 18:03:41 +0400

    Minor updates

---
+++ app/src/main/java/org/michaelbel/app/NotTested.java
@@ -0,0 +1,18 @@
+package org.michaelbel.app;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Date: 06 APR 2018
+ * Time: 18:03 MSK
+ *
+ * @author Michael Bel
+ */
+
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+@NotTested
+@SuppressWarnings("all")
+public @interface NotTested {}
