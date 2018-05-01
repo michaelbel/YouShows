564d4c0968af96bb7e8f1d4014ee8c4edb544b80
Michael Bel <michael-bel@outlook.com>
2018-05-01 23:57:58 +0400

    Remove old events

--- app/src/main/java/org/michaelbel/app/eventbus/Events.java
+++ app/src/main/java/org/michaelbel/app/eventbus/Events.java
@@ -1,45 +1,16 @@
 package org.michaelbel.app.eventbus;
 
-import org.michaelbel.app.NotTested;
-
 public class Events {
 
     public static class UpdateSeasonView {
-
         public UpdateSeasonView() {}
     }
 
-    @NotTested
-    public static class DeleteSeries {
-
-        private String seriesTitle;
-
-        public DeleteSeries(String title) {
-            seriesTitle = title;
-        }
-
-        public String getSeriesTitle() {
-            return seriesTitle;
-        }
+    public static class ChangeDefaultTab {
+        public ChangeDefaultTab() {}
     }
 
-    @NotTested
-    public static class UpdateSeries {
-
-        private String seriesTitle;
-
-        public UpdateSeries(String title) {
-            seriesTitle = title;
-        }
-
-        public String getSeriesTitle() {
-            return seriesTitle;
-        }
-    }
-
-    @NotTested
-    public static class AddSeries {
-
-        public AddSeries() {}
+    public static class EnableSorting {
+        public EnableSorting() {}
     }
 }
