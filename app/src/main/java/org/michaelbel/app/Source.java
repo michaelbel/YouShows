d6606c6a8046cc3301801facb08b7533d534140a
Michael Bel <michael-bel@outlook.com>
2018-05-10 13:10:48 +0400

    Create object for open-source libraries

---
+++ app/src/main/java/org/michaelbel/app/Source.java
@@ -0,0 +1,21 @@
+package org.michaelbel.app;
+
+/**
+ * Date: 08 MAY 2018
+ * Time: 14:20 MSK
+ *
+ * @author Michael Bel
+ */
+
+public class Source {
+
+    public String url;
+    public String name;
+    public String license;
+
+    public Source(String name, String url, String license) {
+        this.url = url;
+        this.name = name;
+        this.license = license;
+    }
+}
