8023056575da71dc430adeb5e83a45fe1681ccb1
Michael Bel <michael-bel@outlook.com>
2018-06-05 10:27:36 +0400

    Deprecated

--- app/src/main/java/org/michaelbel/app/ScreenUtils.java
+++ app/src/main/java/org/michaelbel/app/ScreenUtils.java
@@ -1,7 +1,9 @@
 package org.michaelbel.app;
 
+@Deprecated
 public class ScreenUtils {
 
+    @Deprecated
     public static int dp(float value) {
         return (int) Math.ceil(YouShows.AppContext.getResources().getDisplayMetrics().density * value);
     }
