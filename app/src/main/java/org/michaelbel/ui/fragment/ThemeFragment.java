b8055b895b84425946fe89e406dcea8e241f0ef3
Michael Bel <michael-bel@outlook.com>
2018-06-07 23:36:16 +0400

    Update fragment

--- app/src/main/java/org/michaelbel/ui/fragment/ThemeFragment.java
+++ app/src/main/java/org/michaelbel/ui/fragment/ThemeFragment.java
@@ -1,10 +1,10 @@
 package org.michaelbel.ui.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,10 +15,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-import org.michaelbel.app.YouShows;
+import org.michaelbel.app.AndroidExtensions;
 import org.michaelbel.app.Theme;
+import org.michaelbel.app.YouShows;
 import org.michaelbel.app.eventbus.Events;
-import org.michaelbel.app.LayoutHelper;
 import org.michaelbel.shows.R;
 import org.michaelbel.ui.SettingsActivity;
 import org.michaelbel.ui.view.cell.ThemeCell;
@@ -35,8 +35,8 @@ public class ThemeFragment extends Fragment implements View.OnClickListener {
     private String[] themes;
     private SettingsActivity activity;
 
-    private ThemeCell nightBlueCell;
-    private ThemeCell nightBlackCell;
+    private ThemeCell lightCell;
+    private ThemeCell nightCell;
     private LinearLayout fragmentView;
 
     @Override
@@ -50,118 +50,78 @@ public class ThemeFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         activity.toolbar.setNavigationOnClickListener(view -> activity.finishFragment());
-        activity.toolbarTitle.setText(R.string.Theme);
 
         fragmentView = new LinearLayout(activity);
         fragmentView.setOrientation(LinearLayout.VERTICAL);
         fragmentView.setBackgroundColor(ContextCompat.getColor(activity, Theme.Color.background()));
 
-        nightBlueCell = new ThemeCell(activity, Theme.THEME_NIGHT_BLUE);
-        nightBlueCell.setDivider(true);
-        nightBlueCell.setText(themes[0]);
-        nightBlueCell.setOnClickListener(this);
-        fragmentView.addView(nightBlueCell);
+        lightCell = new ThemeCell(activity, Theme.THEME_LIGHT);
+        lightCell.setDivider(true);
+        lightCell.setText(themes[0]);
+        lightCell.setOnClickListener(this);
+        lightCell.setIconChecked(Theme.getTheme() == Theme.THEME_LIGHT);
+        lightCell.setIcon(Theme.getIcon(R.drawable.ic_theme_light, ContextCompat.getColor(activity, Theme.Color.iconActive())));
+        fragmentView.addView(lightCell);
 
-        nightBlackCell = new ThemeCell(activity, Theme.THEME_NIGHT_BLACK);
-        nightBlackCell.setDivider(false);
-        nightBlackCell.setText(themes[1]);
-        nightBlackCell.setOnClickListener(this);
-        fragmentView.addView(nightBlackCell);
-
-        changeIcon();
+        nightCell = new ThemeCell(activity, Theme.THEME_NIGHT);
+        nightCell.setDivider(false);
+        nightCell.setText(themes[1]);
+        nightCell.setOnClickListener(this);
+        nightCell.setIconChecked(Theme.getTheme() == Theme.THEME_NIGHT);
+        nightCell.setIcon(Theme.getIcon(R.drawable.ic_theme_night, ContextCompat.getColor(activity, Theme.Color.iconActive())));
+        fragmentView.addView(nightCell);
         return fragmentView;
     }
 
     @Override
-    public void onConfigurationChanged(Configuration configuration) {
-        super.onConfigurationChanged(configuration);
-        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            nightBlueCell.setLayoutParams(LayoutHelper.makeLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT, 56, 0, 56, 0));
-            nightBlackCell.setLayoutParams(LayoutHelper.makeLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT, 56, 0, 56, 0));
-        } else {
-            nightBlueCell.setLayoutParams(LayoutHelper.makeLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
-            nightBlackCell.setLayoutParams(LayoutHelper.makeLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
-        }
-    }
-
-    @Override
     public void onClick(View view) {
-        SharedPreferences prefs = activity.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
-
-        if (view == nightBlueCell) {
-            if (Theme.getTheme() != Theme.THEME_NIGHT_BLUE) {
-                prefs.edit().putInt("theme", Theme.THEME_NIGHT_BLUE).apply();
-                changeIcon();
-                changeTheme();
-            }
-        } else if (view == nightBlackCell) {
-            if (Theme.getTheme() != Theme.THEME_NIGHT_BLACK) {
-                prefs.edit().putInt("theme", Theme.THEME_NIGHT_BLACK).apply();
-                changeIcon();
-                changeTheme();
-            }
-        }
+        activity.prefs.edit().putInt("theme", view == lightCell ? Theme.THEME_LIGHT : Theme.THEME_NIGHT).apply();
+        changeCellTheme();
+        changeFragmentTheme();
     }
 
-    private void changeIcon() {
-        nightBlueCell.setIconChecked(Theme.getTheme() == Theme.THEME_NIGHT_BLUE);
-        nightBlackCell.setIconChecked(Theme.getTheme() == Theme.THEME_NIGHT_BLACK);
+    private void changeCellTheme() {
+        lightCell.changeTheme();
+        lightCell.setIconChecked(Theme.getTheme() == Theme.THEME_LIGHT);
+        lightCell.setIcon(Theme.getIcon(R.drawable.ic_theme_light, ContextCompat.getColor(activity, Theme.Color.iconActive())));
+
+        nightCell.changeTheme();
+        nightCell.setIconChecked(Theme.getTheme() == Theme.THEME_NIGHT);
+        nightCell.setIcon(Theme.getIcon(R.drawable.ic_theme_night, ContextCompat.getColor(activity, Theme.Color.iconActive())));
     }
 
-    private void changeTheme() {
-        nightBlueCell.changeTheme();
-        nightBlackCell.changeTheme();
+    private void changeFragmentTheme() {
+        int statusBarColorStart = ContextCompat.getColor(activity, Theme.themeLight() ? R.color.n_primaryDark : R.color.primaryDark);
+        int statusBarColorEnd = ContextCompat.getColor(activity, Theme.themeLight() ? R.color.primaryDark : R.color.n_primaryDark);
 
-        changeStatusBarColor();
-        changeToolbarBackground();
-        changeFragmentViewBackground();
+        int toolbarColorStart = ContextCompat.getColor(activity, Theme.themeLight() ? R.color.n_primary : R.color.primary);
+        int toolbarColorEnd = ContextCompat.getColor(activity, Theme.themeLight() ? R.color.primary : R.color.n_primary);
 
-        ((YouShows) activity.getApplication()).bus().send(new Events.ChangeTheme());
-    }
+        int fragmentColorStart = ContextCompat.getColor(activity, Theme.themeLight() ? R.color.n_background : R.color.background);
+        int fragmentColorEnd = ContextCompat.getColor(activity, Theme.themeLight() ? R.color.background : R.color.n_background);
 
-    private void changeStatusBarColor() {
-        if (Theme.getTheme() == Theme.THEME_NIGHT_BLUE) {
-            activity.getWindow().setStatusBarColor(ContextCompat.getColor(activity, R.color.nb_primaryDark));
-        } else if (Theme.getTheme() == Theme.THEME_NIGHT_BLACK) {
-            activity.getWindow().setStatusBarColor(ContextCompat.getColor(activity, R.color.primaryDark));
-        }
-    }
-
-    private void changeToolbarBackground() {
         ArgbEvaluator evaluator = new ArgbEvaluator();
-        ObjectAnimator fragmentBackgroundAnimator = ObjectAnimator.ofObject(activity.toolbar, "backgroundColor", evaluator, 0, 0);
 
-        int startColor = 0, endColor = 0;
+        ObjectAnimator statusBarAnim = ObjectAnimator.ofObject(activity.getWindow(), "statusBarColor", evaluator, 0, 0);
+        statusBarAnim.setObjectValues(statusBarColorStart, statusBarColorEnd);
 
-        if (Theme.getTheme() == Theme.THEME_NIGHT_BLUE) {
-            startColor = ContextCompat.getColor(activity, R.color.primary);
-            endColor = ContextCompat.getColor(activity, R.color.nb_primary);
-        } else if (Theme.getTheme() == Theme.THEME_NIGHT_BLACK) {
-            startColor = ContextCompat.getColor(activity, R.color.nb_primary);
-            endColor = ContextCompat.getColor(activity, R.color.primary);
-        }
+        ObjectAnimator toolbarAnim = ObjectAnimator.ofObject(activity.toolbar, "backgroundColor", evaluator, 0, 0);
+        toolbarAnim.setObjectValues(toolbarColorStart, toolbarColorEnd);
 
-        fragmentBackgroundAnimator.setObjectValues(startColor, endColor);
-        fragmentBackgroundAnimator.setDuration(300);
-        fragmentBackgroundAnimator.start();
-    }
+        ObjectAnimator fragmentAnim = ObjectAnimator.ofObject(fragmentView, "backgroundColor", evaluator, 0, 0);
+        fragmentAnim.setObjectValues(fragmentColorStart, fragmentColorEnd);
 
-    private void changeFragmentViewBackground() {
-        ArgbEvaluator evaluator = new ArgbEvaluator();
-        ObjectAnimator fragmentBackgroundAnimator = ObjectAnimator.ofObject(fragmentView, "backgroundColor", evaluator, 0, 0);
-
-        int startColor = 0, endColor = 0;
-
-        if (Theme.getTheme() == Theme.THEME_NIGHT_BLUE) {
-            startColor = ContextCompat.getColor(activity, R.color.background);
-            endColor = ContextCompat.getColor(activity, R.color.nb_background);
-        } else if (Theme.getTheme() == Theme.THEME_NIGHT_BLACK) {
-            startColor = ContextCompat.getColor(activity, R.color.nb_background);
-            endColor = ContextCompat.getColor(activity, R.color.background);
-        }
-
-        fragmentBackgroundAnimator.setObjectValues(startColor, endColor);
-        fragmentBackgroundAnimator.setDuration(300);
-        fragmentBackgroundAnimator.start();
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(statusBarAnim, toolbarAnim, fragmentAnim);
+        animatorSet.setDuration(300);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                activity.changeSettingsFragmentTheme();
+                ((YouShows) activity.getApplication()).bus().send(new Events.ChangeTheme());
+            }
+        });
+        AndroidExtensions.runOnUIThread(animatorSet:: start);
     }
 }
