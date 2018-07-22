297f61a5fa1955b1228ad10d6e2289c2ae3c598d
Michael Bel <michael-bel@outlook.com>
2018-07-22 23:05:52 +0400

    Update theme

--- app/src/main/java/org/michaelbel/app/Theme.java
+++ app/src/main/java/org/michaelbel/app/Theme.java
@@ -2,9 +2,6 @@ package org.michaelbel.app;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.DrawableRes;
 
 import org.michaelbel.shows.R;
 
@@ -17,284 +14,143 @@ import org.michaelbel.shows.R;
 
 public class Theme {
 
-    public static final int THEME_NIGHT_BLUE = 0;
-    public static final int THEME_NIGHT_BLACK = 1;
-    public static final int THEME_NIGHT_RED = 2;
-
-    private static Context getContext() {
-        return YouShows.AppContext;
-    }
+    public static final int THEME_LIGHT = 0;
+    public static final int THEME_NIGHT = 1;
 
     public static int getTheme() {
-        SharedPreferences prefs = getContext().getSharedPreferences("mainconfig", Context.MODE_PRIVATE);
-        return prefs.getInt("theme", 1);
+        SharedPreferences prefs = YouShows.AppContext.getSharedPreferences("mainconfig", Context.MODE_PRIVATE);
+        return prefs.getInt("theme", THEME_LIGHT);
     }
 
-    public static class Color {
-
-        public static int primary() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_primary;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.primary;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_primary;
-            }
-
-            return 0;
-        }
-
-        public static int primaryDark() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_primaryDark;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.primaryDark;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_primaryDark;
-            }
-
-            return 0;
-        }
-
-        public static int accent() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_accent;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.accent;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_accent;
-            }
-
-            return 0;
-        }
-
-        public static int primaryText() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_primaryText;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.primaryText;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_primaryText;
-            }
-
-            return 0;
-        }
-
-        public static int secondaryText() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_secondaryText;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.secondaryText;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_secondaryText;
-            }
-
-            return 0;
-        }
-
-        public static int disabledHintText() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_disabledHintText;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.disabledHintText;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_disabledHintText;
-            }
-
-            return 0;
-        }
-
-        public static int divider() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_divider;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.divider;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_divider;
-            }
-
-            return 0;
-        }
-
-        public static int iconActive() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_iconActive;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.iconActive;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_iconActive;
-            }
-
-            return 0;
-        }
-
-        public static int iconInactive() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_iconInactive;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.iconInactive;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_iconInactive;
-            }
-
-            return 0;
-        }
-
-        public static int statusBar() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_statusBar;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.statusBar;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_statusBar;
-            }
-
-            return 0;
-        }
-
-        public static int appBar() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_appBar;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.appBar;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_appBar;
-            }
-
-            return 0;
-        }
-
-        public static int background() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_background;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.background;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_background;
-            }
-
-            return 0;
-        }
-
-        public static int foreground() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_foreground;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.foreground;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_foreground;
-            }
-
-            return 0;
-        }
-
-        public static int thumbOn() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_switch_thumbOn;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.switch_thumbOn;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_switch_thumbOn;
-            }
-
-            return 0;
-        }
-
-        public static int thumbOff() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_switch_thumbOff;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.switch_thumbOff;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_switch_thumbOff;
-            }
-
-            return 0;
-        }
-
-        public static int trackOn() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_switch_trackOn;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.switch_trackOn;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_switch_trackOn;
-            }
-
-            return 0;
-        }
-
-        public static int trackOff() {
-            if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-                return R.color.nb_switch_trackOff;
-            } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-                return R.color.switch_trackOff;
-            } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-                return R.color.nr_switch_trackOff;
-            }
-
-            return 0;
-        }
+    public static boolean themeLight() {
+        return getTheme() == THEME_LIGHT;
     }
 
-    public static int alertTheme() {
-        if (Theme.getTheme() == THEME_NIGHT_BLUE) {
-            return R.style.AlertNightBlue;
-        } else if (Theme.getTheme() == THEME_NIGHT_BLACK) {
-            return R.style.AlertNightBlack;
-        } else if (Theme.getTheme() == THEME_NIGHT_RED) {
-            return R.style.AlertNightRed;
-        }
+//--Styles------------------------------------------------------------------------------------------
 
-        return 0;
+    public static int alertDialogStyle() {
+        return themeLight() ? R.style.AlertLight : R.style.AlertNight;
     }
 
-    public static Drawable getIcon(@DrawableRes int resource, int colorFilter) {
-        return getIcon(resource, colorFilter, PorterDuff.Mode.MULTIPLY);
+//--Colors------------------------------------------------------------------------------------------
+
+    public static int primaryColor() {
+        return themeLight() ? R.color.primary : R.color.n_primary;
     }
 
-    public static Drawable getIcon(@DrawableRes int resource, int colorFilter, PorterDuff.Mode mode) {
-        Drawable iconDrawable = getContext().getResources().getDrawable(resource, null);
-
-        if (iconDrawable != null) {
-            iconDrawable.clearColorFilter();
-            iconDrawable.mutate().setColorFilter(colorFilter, mode);
-        }
-
-        return iconDrawable;
+    public static int primaryDarkColor() {
+        return themeLight() ? R.color.primaryDark : R.color.n_primaryDark;
     }
 
-    /*
-    public static int getAttrColor(@NonNull Context context, @AttrRes int colorAttr) {
-        int color = 0;
-        int[] attrs = new int[] {
-                colorAttr
-        };
-
-        try {
-            TypedArray typedArray = context.obtainStyledAttributes(attrs);
-            color = typedArray.getColor(0, 0);
-            typedArray.recycle();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return color;
+    public static int accentColor() {
+        return themeLight() ? R.color.accent : R.color.n_accent;
     }
 
-    public static int[] getColorArray(@NonNull Context context, @ArrayRes int arrayRes) {
-        if (arrayRes == 0) {
-            return null;
-        }
+    public static int backgroundColor() {
+        return themeLight() ? R.color.background : R.color.n_background;
+    }
 
-        TypedArray ta = context.getResources().obtainTypedArray(arrayRes);
-        int[] colors = new int[ta.length()];
+    public static int foregroundColor() {
+        return themeLight() ? R.color.foreground : R.color.n_foreground;
+    }
 
-        for (int i = 0; i < ta.length(); i++) {
-            colors[i] = ta.getColor(i, 0);
-        }
+    public static int primaryTextColor() {
+        return themeLight() ? R.color.primaryText : R.color.n_primaryText;
+    }
 
-        ta.recycle();
-        return colors;
-    }*/
+    public static int secondaryTextColor() {
+        return themeLight() ? R.color.secondaryText : R.color.n_secondaryText;
+    }
+
+    public static int iconActiveColor() {
+        return themeLight() ? R.color.iconActive : R.color.n_iconActive;
+    }
+
+    public static int iconInactiveColor() {
+        return themeLight() ? R.color.iconInactive : R.color.n_iconInactive;
+    }
+
+    public static int statusBarColor() {
+        return themeLight() ? R.color.statusBar : R.color.n_statusBar;
+    }
+
+    public static int appBarColor() {
+        return themeLight() ? R.color.appBar : R.color.n_appBar;
+    }
+
+    public static int dividerColor() {
+        return themeLight() ? R.color.divider : R.color.n_divider;
+    }
+
+    public static int thumbOnColor() {
+        return themeLight() ? R.color.switch_thumbOn : R.color.n_switch_thumbOn;
+    }
+
+    public static int thumbOffColor() {
+        return themeLight() ? R.color.switch_thumbOff : R.color.n_switch_thumbOff;
+    }
+
+    public static int trackOnColor() {
+        return themeLight() ? R.color.switch_trackOn : R.color.n_switch_trackOn;
+    }
+
+    public static int trackOffColor() {
+        return themeLight() ? R.color.switch_trackOff : R.color.n_switch_trackOff;
+    }
+
+//--Other colors------------------------------------------------------------------------------------
+
+    public static int fabShowFollowColor() {
+        return themeLight() ? R.color.white : R.color.n_accent;
+    }
+
+    public static int fabShowFollowIconColor() {
+        return themeLight() ? R.color.iconActive : R.color.white;
+    }
+
+    public static int showNameColor() {
+        return themeLight() ? R.color.primaryDark : R.color.n_accent;
+    }
+
+    public static int swipeRefreshProgressColor() {
+        return themeLight() ? R.color.white : R.color.n_iconActive;
+    }
+
+    public static int tabSelectColor() {
+        return themeLight() ? R.color.white : R.color.accent;
+    }
+
+    public static int tabUnselectColor() {
+        return themeLight() ? R.color.tabUnselected : R.color.n_tabUnselected;
+    }
+
+    public static int changelogCurrentVersionText() {
+        return themeLight() ? R.color.green_dark : R.color.green;
+    }
+
+    public static int seasonViewBackground() {
+        return themeLight() ? R.color.background : R.color.n_background;
+    }
+
+    public static int background() {
+        return themeLight() ? R.color.background : R.color.n_background;
+    }
+
+    public static int dialogButtonText() {
+        return themeLight() ? R.color.n_accent : R.color.accent;
+    }
+
+    public static int changelogVersionText() {
+        return themeLight() ? R.color.blue : R.color.yellow;
+    }
+
+    public static int sortDivider() {
+        return themeLight() ? R.color.sortDivider : R.color.n_background;
+    }
+
+//--Resources---------------------------------------------------------------------------------------
+
+    public static int fabProgressBar() {
+        return themeLight() ? R.drawable.ic_anim_progressbar2 : R.drawable.ic_anim_progressbar;
+    }
 }
